17135f648e10677593a20c283183449f50893291
chriswifn <christian.hageloch@gmail.com>
2022-02-20 16:30:47 +0100

    nvim and doom emacs

---
+++ .config/nvim/lua/user/settings.lua
@@ -0,0 +1,68 @@
+local opts = { noremap = true, silent = true }
+local map = vim.api.nvim_set_keymap
+local set = vim.opt
+
+local config = {
+
+ -- Set colorscheme
+  --colorscheme = "OceanicNext",
+  colorscheme = "OceanicNext",
+  -- Add plugins
+  plugins = {
+    { "mhartington/oceanic-next"},
+    -- {
+    -- "ray-x/lsp_signature.nvim",
+    -- event = "BufRead",
+    -- config = function()
+    -- require("lsp_signature").setup()
+    -- end,
+    -- },
+  },
+
+  overrides = {
+    treesitter = {
+      ensure_installed = { "lua" },
+    },
+  },
+
+  -- On/off virtual diagnostics text
+  virtual_text = true,
+
+  -- Disable default plugins
+  enabled = {
+    bufferline = true,
+    nvim_tree = true,
+    lualine = true,
+    lspsaga = true,
+    gitsigns = true,
+    colorizer = true,
+    toggle_term = true,
+    comment = true,
+    symbols_outline = true,
+    indent_blankline = true,
+    dashboard = true,
+    which_key = true,
+    neoscroll = true,
+    ts_rainbow = true,
+    ts_autotag = true,
+  },
+}
+
+-- Set options
+set.relativenumber = true
+
+vim.cmd('set wrap')
+vim.cmd([[let g:vimtex_view_method = 'zathura']])
+
+-- Set key bindings
+map("n", "<C-s>", ":w!<CR>", opts)
+
+-- Set autocommands
+vim.cmd [[
+  augroup packer_conf
+    autocmd!
+    autocmd bufwritepost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+return config
